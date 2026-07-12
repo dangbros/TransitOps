099ce91b7c992ed57444269ff81d9fdb6d6febda
Protyush <ghorui.protyushraj@gmail.com>
2026-07-12 14:43:06 +0530

    feat(layout): integrate react-hot-toast notifications container into dashboard layout wrapper

--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
 import Header from "../components/Header";
+import { Toaster } from "react-hot-toast";
 
 const DashboardLayout = () => {
   return (
     <div className="flex min-h-screen bg-slate-50">
+      <Toaster position="top-right" reverseOrder={false} />
       <Sidebar />
       <div className="flex-1 flex flex-col">
         <Header />
